66cc78cd9dcdc450cfdc1f70dede86aadb4aff89
Paweł Czyż <pczyz@protonmail.com>
2022-09-08 14:35:54 +0200

    Sketch the repository structure and the main interfaces (#3)
    * Sketch the repository structure and the main interfaces.
    
    * Add MI to the interface.
    
    * Change from Tuple to tuple
    
    * Rename x_samples to x (and similarly for y_samples)
    
    * Update interface.py
    
    now explicit subtyping will raise not implemented errors and provide dim_total for free
    
    Co-authored-by: Frederic Grabowski <grabowski.frederic@gmail.com>

---
+++ src/bmi/interface.py
@@ -0,0 +1,65 @@
+"""Most important interfaces of the package."""
+from typing import Any, Protocol
+from abc import abstractmethod
+
+import numpy as np
+from numpy.typing import ArrayLike  # pytype: disable=import-error
+
+
+class IMutualInformationPointEstimator(Protocol):
+    """Interface for the mutual information estimator."""
+    
+    @abstractmethod
+    def estimate(self, x: ArrayLike, y: ArrayLike) -> float:
+        """A point estimate of MI(X; Y) from an i.i.d sample from the P(X, Y) distribution.
+
+        Args:
+            x: shape (n_samples, dim_x)
+            y: shape (n_samples, dim_y)
+
+        Returns:
+            mutual information estimate
+        """
+        raise NotImplementedError
+
+
+class IDistribution(Protocol):
+    """Interface for a distribution P(X, Y)."""
+
+    @abstractmethod
+    def sample(self, n_points: int, rng: Any) -> tuple[np.ndarray, np.ndarray]:
+        """Returns a sample from the joint distribution P(X, Y).
+
+        Args:
+            n_points: sample size
+            rng: pseudorandom number generator
+
+        Returns:
+            X samples, shape (n_points, dim_x)
+            Y samples, shape (n_points, dim_y). Note that these samples are paired with X samples.
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def dim_x(self) -> int:
+        """Dimension of the space in which the `X` variable is valued."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def dim_y(self) -> int:
+        """Dimension of the space in which the `Y` variable is valued."""
+        raise NotImplementedError
+
+    @property
+    def dim_total(self) -> int:
+        """Dimension of the space in which the `(X, Y)` variable is valued.
+        Should be equal to the sum of `dim_x` and `dim_y`.
+        """
+        return self.dim_x + self.dim_y
+
+    @abstractmethod
+    def mutual_information(self) -> float:
+        """Mutual information MI(X; Y)."""
+        raise NotImplementedError
