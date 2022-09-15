b3d04048cb4389268bf68de93b10789fe0347614
Paweł Czyż <pczyz@protonmail.com>
2022-09-15 10:01:16 +0200

    Generalize KSG estimator

--- src/bmi/estimators/ksg.py
+++ src/bmi/estimators/ksg.py
@@ -1,5 +1,5 @@
 """Kraskov estimators."""
-from typing import Sequence, cast
+from typing import Literal, Optional, Sequence, cast
 
 import numpy as np
 from numpy.typing import ArrayLike
@@ -9,23 +9,47 @@ from sklearn import metrics, preprocessing
 from bmi.estimators.base import EstimatorNotFittedException
 from bmi.interface import IMutualInformationPointEstimator
 
+_AllowedContinuousMetric = Literal["euclidean", "manhattan", "chebyshev", "mahalonobis"]
+
 
 class KSGEnsembleFirstEstimator(IMutualInformationPointEstimator):
     """Ensemble estimator built using the first approximation (equation (8) in the paper)."""
 
-    def __init__(self, neighborhoods: Sequence[int] = (5, 10), standardize: bool = True) -> None:
+    def __init__(
+        self,
+        neighborhoods: Sequence[int] = (5, 10),
+        standardize: bool = True,
+        metric_x: _AllowedContinuousMetric = "euclidean",
+        metric_y: Optional[_AllowedContinuousMetric] = None,
+        n_jobs: int = 1,
+    ) -> None:
         """
 
         Args:
             neighborhoods: sequence of positive integers,
               specifying the size of neighborhood for MI calculation
             standardize: whether to standardize the data before MI calculation, by default true
+            metric_x: metric on the X space
+            metric_y: metric on the Y space. If None, `metric_x` will be used
+            n_jobs: number of jobs to be launched to compute distances.
+              Use -1 to use all processors.
+
+        Note:
+            If you use Chebyshev (l-infinity) distance for both X and Y,
+            `KSGChebyshevEstimator` may be faster.
         """
         if min(neighborhoods) < 1:
             raise ValueError("Each neighborhood must be at least 1.")
 
         self._neighborhoods = list(neighborhoods)
         self._standardize = standardize
+
+        self._n_jobs: int = n_jobs
+
+        self._metric_x: _AllowedContinuousMetric = metric_x
+        self._metric_y: _AllowedContinuousMetric = (
+            metric_y or metric_x
+        )  # If `metric_y` is None, use `metric_x`
 
         self._fitted = False
         self._mi_dict = dict()  # set by fit()
@@ -50,8 +74,12 @@ class KSGEnsembleFirstEstimator(IMutualInformationPointEstimator):
         n_points = np.shape(x)[0]
         for index in range(n_points):
             # Distances from x[index] to all the points:
-            distances_x = metrics.pairwise_distances(x[None, index], x)[0, :]
-            distances_y = metrics.pairwise_distances(y[None, index], y)[0, :]
+            distances_x = metrics.pairwise_distances(
+                x[None, index], x, metric=self._metric_x, n_jobs=self._n_jobs
+            )[0, :]
+            distances_y = metrics.pairwise_distances(
+                y[None, index], y, metric=self._metric_y, n_jobs=self._n_jobs
+            )[0, :]
 
             # In the product (XxY) space we use the maximum distance
             distances_z = np.maximum(distances_x, distances_y)
@@ -87,3 +115,11 @@ class KSGEnsembleFirstEstimator(IMutualInformationPointEstimator):
         self.fit(x, y)
         predictions = np.asarray(list(self.get_predictions().values()))
         return cast(float, np.mean(predictions))
+
+
+class KSGEnsembleChebyshevEstimator(IMutualInformationPointEstimator):
+    """Mutual information estimator based on fast nearest neighbours,
+    available when Chebyshev (l-infty) metric is used for both X and Y spaces.
+    """
+
+    raise NotImplementedError
