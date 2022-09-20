4c6c9bb11bf9b355ffd78c999f47d49e8cdc46b6
Paweł Czyż <pczyz@protonmail.com>
2022-09-20 21:36:30 +0200

    Add the old estimator.

--- src/bmi/estimators/ksg.py
+++ src/bmi/estimators/ksg.py
@@ -127,7 +127,7 @@ class KSGEnsembleFirstEstimator(IMutualInformationPointEstimator):
         metric_x: _AllowedContinuousMetric = "euclidean",
         metric_y: Optional[_AllowedContinuousMetric] = None,
         n_jobs: int = 1,
-        chunk_size: int = 100,
+        chunk_size: int = 10,
     ) -> None:
         """
 
@@ -212,6 +212,102 @@ class KSGEnsembleFirstEstimator(IMutualInformationPointEstimator):
             # As the mean over all the points was calculated for each chunk separately,
             # we should add the contributions from each chunk
             mi_estimate = _DIGAMMA(k) - np.sum(digammas) + _DIGAMMA(n_points)
+            self._mi_dict[k] = max(0.0, mi_estimate)
+
+        self._fitted = True
+
+    def get_predictions(self) -> dict[int, float]:
+        if not self._fitted:
+            raise EstimatorNotFittedException
+        return cast(dict[int, float], self._mi_dict.copy())
+
+    def estimate(self, x: ArrayLike, y: ArrayLike) -> float:
+        self.fit(x, y)
+        predictions = np.asarray(list(self.get_predictions().values()))
+        return cast(float, np.mean(predictions))
+
+
+class KSGEnsembleFirstEstimatorSlow(IMutualInformationPointEstimator):
+    """Ensemble estimator built using the first approximation (equation (8) in the paper).
+
+    Note:
+        `KSGEnsembleFirstEstimator` is a faster, NumPy-based, version of this one.
+        We decided to retain it for instructional purposes (as it's easier to read)
+    """
+
+    def __init__(
+        self,
+        neighborhoods: Sequence[int] = (5, 10),
+        standardize: bool = True,
+        metric_x: _AllowedContinuousMetric = "euclidean",
+        metric_y: Optional[_AllowedContinuousMetric] = None,
+        n_jobs: int = 1,
+    ) -> None:
+        """
+        Args:
+            neighborhoods: sequence of positive integers,
+              specifying the size of neighborhood for MI calculation
+            standardize: whether to standardize the data before MI calculation, by default true
+            metric_x: metric on the X space
+            metric_y: metric on the Y space. If None, `metric_x` will be used
+            n_jobs: number of jobs to be launched to compute distances.
+              Use -1 to use all processors.
+        """
+        self._neighborhoods = _cast_and_check_neighborhoods(neighborhoods)
+        self._standardize = standardize
+
+        self._n_jobs: int = n_jobs
+
+        self._metric_x: _AllowedContinuousMetric = metric_x
+        self._metric_y: _AllowedContinuousMetric = (
+            metric_y or metric_x
+        )  # If `metric_y` is None, use `metric_x`
+
+        self._fitted = False
+        self._mi_dict = dict()  # set by fit()
+
+    def fit(self, x: ArrayLike, y: ArrayLike) -> None:
+        space = _ProductSpace(x=x, y=y, standardize=self._standardize)
+
+        n_points = len(space)
+        if n_points <= max(self._neighborhoods):
+            raise ValueError(
+                f"Maximum neighborhood used is {max(self._neighborhoods)} "
+                f"but the number of points provided is only {n_points}."
+            )
+
+        digammas_dict = {k: [] for k in self._neighborhoods}
+
+        for index in range(n_points):
+            # Distances from x[index] to all the points:
+            distances_x = metrics.pairwise_distances(
+                space.x[None, index], space.x, metric=self._metric_x, n_jobs=self._n_jobs
+            )[0, :]
+            distances_y = metrics.pairwise_distances(
+                space.y[None, index], space.y, metric=self._metric_y, n_jobs=self._n_jobs
+            )[0, :]
+
+            # In the product (XxY) space we use the maximum distance
+            distances_z = np.maximum(distances_x, distances_y)
+            # And we sort the point indices by being the closest to the considered one
+            closest_points = sorted(range(len(distances_z)), key=lambda i: distances_z[i])
+
+            for k in self._neighborhoods:
+                # Note that the points are 0-indexed and that the "0th neighbor"
+                # is the point itself (as distance(x, x) = 0 is the smallest possible)
+                # Hence, the kth neighbour is at index k
+                kth_neighbour = closest_points[k]
+                distance = distances_z[kth_neighbour]
+
+                # Don't include the `i`th point itself in n_x and n_y
+                n_x = (distances_x < distance).sum() - 1
+                n_y = (distances_y < distance).sum() - 1
+
+                digammas_per_point = _DIGAMMA(n_x + 1) + _DIGAMMA(n_y + 1)
+                digammas_dict[k].append(digammas_per_point)
+
+        for k, digammas in digammas_dict.items():
+            mi_estimate = _DIGAMMA(k) - np.mean(digammas) + _DIGAMMA(n_points)
             self._mi_dict[k] = max(0.0, mi_estimate)
 
         self._fitted = True
