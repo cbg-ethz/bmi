3a2934d974c97fc444e9392f6caceb8ca5a0c843
Paweł Czyż <pczyz@protonmail.com>
2022-11-17 21:24:21 +0100

    Add CLI to the script.

--- scripts/ksg_spiral.py
+++ scripts/ksg_spiral.py
@@ -1,13 +1,30 @@
+import argparse
+
 import numpy as np
 
 import bmi.api as bmi
 import bmi.transforms.rotate as rot
 
 
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Experiment with applying the spiral diffeomorphism."
+    )
+    parser.add_argument("--dim-x", type=int, default=3, help="Dimension of the X variable.")
+    parser.add_argument("--dim-y", type=int, default=2, help="Dimension of the Y variable.")
+    parser.add_argument("--rho", type=float, default=0.8, help="Correlation, between -1 and 1.")
+    parser.add_argument(
+        "--n-points", type=int, default=5000, help="Number of points to be generated."
+    )
+    parser.add_argument("--seed", type=int, default=42, help="Random seed.")
+    return parser
+
+
 def generate_covariance(correlation: float, dim_x: int, dim_y: int) -> np.ndarray:
     """The correlation between the first dimension of X and the first dimension of Y is fixed.
 
-    The rest of the covariance entries are zero.
+    The rest of the covariance entries are zero,
+    of course except for variance of each dimension (the diagonal), which is 1.
     """
     covariance = np.eye(dim_x + dim_y)
     covariance[0, dim_x] = correlation
@@ -15,31 +32,32 @@ def generate_covariance(correlation: float, dim_x: int, dim_y: int) -> np.ndarra
     return covariance
 
 
-def main() -> None:
-    dim_x = 5
-    dim_y = 2
-    rho = 0.5
-    n_points = 5000
-    seed = 42
-
-    assert 0 <= rho < 1
+def create_base_sampler(dim_x: int, dim_y: int, rho: float) -> bmi.samplers.SplitMultinormal:
+    assert -1 <= rho < 1
 
     covariance = generate_covariance(dim_x=dim_x, dim_y=dim_y, correlation=rho)
 
-    base_sampler = bmi.samplers.SplitMultinormal(
+    return bmi.samplers.SplitMultinormal(
         dim_x=dim_x,
         dim_y=dim_y,
         covariance=covariance,
     )
 
-    x_normal, y_normal = base_sampler.sample(n_points, rng=seed)
+
+def main() -> None:
+    parser = create_parser()
+    args = parser.parse_args()
+
+    base_sampler = create_base_sampler(dim_x=args.dim_x, dim_y=args.dim_y, rho=args.rho)
+
+    x_normal, y_normal = base_sampler.sample(args.n_points, rng=args.seed)
     mi_true = base_sampler.mutual_information()
 
     mi_estimate_normal = bmi.estimators.KSGEnsembleFirstEstimator().estimate(x_normal, y_normal)
 
     print(f"{mi_true = :.3f}\t{mi_estimate_normal = :.3f}")
 
-    generator = rot.so_generator(dim_x, i=0, j=1)
+    generator = rot.so_generator(args.dim_x, i=0, j=1)
 
     for speed in [0.0, 0.02, 0.1, 0.5, 1.0, 10.0]:
         transform_x = rot.Spiral(generator=generator, speed=speed)
