542c6315abfe85cd0c62915921409cfad203e5fa
Paweł Czyż <pczyz@protonmail.com>
2022-12-01 20:00:42 +0100

    Add documentation to the external estimator wrapper.

--- src/bmi/benchmark/wrapper.py
+++ src/bmi/benchmark/wrapper.py
@@ -1,9 +1,11 @@
+"""Allows running an external estimator."""
 import subprocess
-from typing import Sequence
+from typing import Optional, Sequence
 
 from bmi.benchmark._serialize import TaskDirectory
 from bmi.benchmark.core import RunResult, TaskMetadata
 from bmi.benchmark.timer import Timer
+from bmi.interface import Pathlike
 
 
 def _run_command_and_read_mi(args: list[str]) -> float:
@@ -20,12 +22,33 @@ def _run_command_and_read_mi(args: list[str]) -> float:
 
 
 def run_external_estimator(
-    task_path, seed: int, command_args: Sequence[str], estimator_id: str
+    task_path: Pathlike,
+    seed: int,
+    command_args: Sequence[str],
+    estimator_id: str,
+    estimator_params: Optional[dict] = None,
 ) -> RunResult:
-    """TODO(Pawel): Add the docstring.
+    """Runs an external estimator via a command line.
 
     Args:
-        task_path: the directory
+        task_path: path to the directory with a given task
+        seed: random seed to select the right samples
+        command_args: arguments necessary to run the estimator
+          (without the parameters dependent on the task,
+          i.e., the samples CSV, seed, and dimensions)
+        estimator_id: the unique identifier of the estimator (and its parameters)
+        estimator_params: dictionary with parameters of the estimator, to be added to the RunResult
+
+    Note:
+        The estimator should take the following CLI arguments:
+        COMMAND ARG_1 ... ARG_N SAMPLES_CSV SEED DIM_X DIM_Y
+        and *print a single float* to the standard output.
+
+        For example, an estimator `some_estimator.sh` which takes
+        one hyperparameter (in this case 1524)
+        can be run on task1/samples.csv with seed 42
+        and with X dimension 5 and Y dimension 2 by:
+        some_estimator.sh 1524 task1/samples.csv 42 5 2
     """
     task_directory = TaskDirectory(task_path)
     metadata = TaskMetadata(**task_directory.load_metadata())
@@ -40,17 +63,11 @@ def run_external_estimator(
     timer = Timer()
     mi_estimate = _run_command_and_read_mi(list(command_args) + our_args)
 
-    # TODO(Pawel): Add handling params and measure time
     return RunResult(
         task_id=metadata.task_id,
+        seed=seed,
         estimator_id=estimator_id,
         mi_estimate=mi_estimate,
-        seed=seed,
         time_in_seconds=timer.check(),
+        estimator_params=estimator_params,
     )
-
-
-"""
-$ julia my_estimator.jl samples.csv 3 [dim_x] [dim_y]
-0.12
-"""
