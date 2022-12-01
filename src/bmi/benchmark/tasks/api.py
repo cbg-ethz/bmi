351ae9f4f9946f0f87fd5aa2754e8ef5082f2c81
Frederic Grabowski <grabowski.frederic@gmail.com>
2022-12-01 12:40:10 +0100

    tasks

---
+++ src/bmi/benchmark/tasks/api.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Iterable
+
+import bmi.benchmark.tasks.multinormal as mn
+from bmi.benchmark.core import Task
+from bmi.interface import Pathlike
+
+BENCHMARK_TASKS = (
+    mn.task_mn_1_2_eye_100,
+    mn.task_mn_1_2_eye_1000,
+)
+
+
+def save_benchmark_tasks(
+    tasks_dir: Pathlike,
+    tasks: Iterable[Task] = BENCHMARK_TASKS,
+    exist_ok: bool = False,
+):
+    for task in tasks:
+        task_dir = Path(tasks_dir) / task.task_id
+        task.save(task_dir, exist_ok=exist_ok)
