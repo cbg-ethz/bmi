345cbc5deb93a2f87df010ca3c65680c479ff420
Paweł Czyż <pczyz@protonmail.com>
2022-12-10 23:18:53 +0100

    Add the LNN estimator, improve documentation.

--- external/rmi.R
+++ external/rmi.R
@@ -1,6 +1,11 @@
-#  Use as
+# Wrapper script for estimating mutual information.
+#
+# Use as:
 # $ Rscript SEED FILE DIMX DIMY [optional hyperparameters]
-library("rmi")
+#
+# To install the requirements run:
+# $ R
+# > install.packages("argparse", "dplyr", "rmi")
 
 parser <- argparse::ArgumentParser()
 
@@ -8,9 +13,10 @@ parser$add_argument("SEED", type="integer", help="Seed to be taken.")
 parser$add_argument("FILE", help="Path to the CSV file.")
 parser$add_argument("DIMX", type="integer", help="Dimension of the X variable.")
 parser$add_argument("DIMY", type="integer", help="Dimension of the Y variable.")
-parser$add_argument("--method", default="KSG1", help="Method to be used. Allowed: KSG1 KSG2 LNC")
+parser$add_argument("--method", default="KSG1", help="Method to be used. Allowed: KSG1, KSG2, LNN, LNC.")
 parser$add_argument("--neighbors", type="integer", default=10, help="Number of neighbors (k) to be used.")
 parser$add_argument("--alpha", type="double", default=0.65, help="Hyperparameter of the LNC method. It's ignored for KSG methods.")
+parser$add_argument("--truncation", type="integer", default=30, help="Order of truncation for the LNN method.")
 
 # Parse arguments
 args <- parser$parse_args()
@@ -32,12 +38,15 @@ data_matrix <- data.matrix(data)
 # Construct the right list of hyperparameters
 if (args$method == "LNC") {
     options <- list(method = args$method, k = args$neighbors, alpha = args$alpha)
-} else {
-    # TODO(Pawel): Check whether it's KSG1 or KSG2. Otherwise, raise an exception.
+    mi_estimate <- rmi::knn_mi(data_matrix, splits, options)
+} else if (args$method == "KSG1" || args$method == "KSG2") {
     options <- list(method = args$method, k = args$neighbors)
+    mi_estimate <- rmi::knn_mi(data_matrix, splits, options)
+} else if (args$method == "LNN"){
+    mi_estimate <- rmi::lnn_mi(data_matrix, splits, k = args$neighbors, tr = args$truncation)
+} else {
+    stop(paste0("Method ", args$method, " not recognized."))
 }
-
-mi_estimate <- rmi::knn_mi(data_matrix, splits, options)
 
 cat(mi_estimate)
 cat("\n")
