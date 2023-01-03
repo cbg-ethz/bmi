26aacb27192a9327b87deb3fa8acfec704eed24e
Paweł Czyż <pczyz@protonmail.com>
2023-01-03 14:42:02 +0100

    Use CCA to estimate MI (#38)

--- src/bmi/samplers/api.py
+++ src/bmi/samplers/api.py
@@ -1,10 +1,10 @@
-from bmi.samplers.dispersion import one_and_one_correlation_matrix
+from bmi.samplers.dispersion import parametrised_correlation_matrix
 from bmi.samplers.split_student_t import SplitStudentT
 from bmi.samplers.splitmultinormal import BivariateNormalSampler, SplitMultinormal
 from bmi.samplers.transformed import TransformedSampler
 
 __all__ = [
-    "one_and_one_correlation_matrix",
+    "parametrised_correlation_matrix",
     "BivariateNormalSampler",
     "SplitMultinormal",
     "SplitStudentT",
