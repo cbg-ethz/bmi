f69b1dfb10a2d03e996d0944cb4526a6348f7960
Paweł Czyż <pczyz@protonmail.com>
2023-01-07 22:38:50 +0100

    Refactor the experimental design.

--- scripts/figures/spiral/experimental_design.py
+++ scripts/figures/spiral/experimental_design.py
@@ -8,6 +8,12 @@ def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("TASKS", type=Path, help="Directory with the tasks.")
     parser.add_argument("RESULTS", type=Path, help="Directory where the results should be dumped.")
+    parser.add_argument(
+        "--summary",
+        default=False,
+        action="store_true",
+        help="Instead of printing experimental design," "print some summary. ",
+    )
     return parser
 
 
@@ -27,10 +33,18 @@ def main() -> None:
 
     results_directory.mkdir(exist_ok=True)
 
+    # Variables for the summary
+    n_runs = 0  # Keeps the total number of runs
+    task_ids = []  # Keeps task IDs
+
     for task_path in task_directory.iterdir():
         task = bmi.benchmark.Task.load(task_path)
+        task_ids.append(task.task_id)
+
         for seed in task.keys():
             for estimator_id, estimator_args in ESTIMATORS.items():
+                n_runs += 1
+
                 # We hash task ID as it may contain spaces or other special characters, which
                 # are not bash-friendly
                 task_id_hash = str(hash(task.task_id))
@@ -44,7 +58,19 @@ def main() -> None:
                     f"--estimator-id {estimator_id} "
                     f"{estimator_args}"
                 )
-                print(command)
+                # If we just want to summarize the experimental design,
+                # we don't print out the commands
+                if not args.summary:
+                    print(command)
+
+    if args.summary:
+        print(f"Total number of runs: {n_runs}")
+        print(f"Total number of tasks: {len(task_ids)}")
+        for task_id in task_ids:
+            print(f"  {task_id}")
+        print("Estimators (ID and additional arguments):")
+        for estimator_id, estimator_args in ESTIMATORS.items():
+            print(f"  {estimator_id}: {estimator_args}")
 
 
 if __name__ == "__main__":
