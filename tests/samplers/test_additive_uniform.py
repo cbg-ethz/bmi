7138cf15f35dfadcb2d8f4c95eea0279f9e23abe
Paweł Czyż <pczyz@protonmail.com>
2023-01-11 14:34:29 +0100

    A prototype for the uniform variable and the uniform additive noise model (#28)

---
+++ tests/samplers/test_additive_uniform.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+from jax import random
+from sklearn.feature_selection import mutual_info_regression
+
+from bmi.samplers.additive_uniform import AdditiveUniformSampler
+
+
+@pytest.mark.parametrize("epsilon", [0.5, 0.1, 2.0, 5.0])
+@pytest.mark.parametrize("rng", [2, random.PRNGKey(32), np.int32(12)])
+def test_uniform_additive(epsilon: float, rng, n_samples: int = 5_000) -> None:
+    sampler = AdditiveUniformSampler(epsilon=epsilon)
+
+    x, y = sampler.sample(n_points=n_samples, rng=rng)
+
+    assert x.shape == (n_samples, 1)
+    assert y.shape == (n_samples, 1)
+
+    mi_estimate = mutual_info_regression(x, y.ravel())[0]
+    assert mi_estimate == pytest.approx(sampler.mutual_information(), rel=0.02, abs=0.02)
