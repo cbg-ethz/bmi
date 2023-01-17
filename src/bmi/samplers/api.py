8d418c25cbcc1d801e0ec47ec0acd4e40eec624b
Paweł Czyż <pczyz@protonmail.com>
2023-01-17 16:51:49 +0100

    Script visualising the distributions.

--- src/bmi/samplers/api.py
+++ src/bmi/samplers/api.py
@@ -2,12 +2,22 @@ from bmi.samplers.additive_uniform import AdditiveUniformSampler
 from bmi.samplers.dispersion import parametrised_correlation_matrix
 from bmi.samplers.split_student_t import SplitStudentT
 from bmi.samplers.splitmultinormal import BivariateNormalSampler, SplitMultinormal
-from bmi.samplers.transformed import TransformedSampler
+
+# isort: off
+from bmi.samplers.transformed import (
+    BivariateUniformMarginsSampler,
+    SwissRollSampler,
+    TransformedSampler,
+)
+
+# isort: on
 
 __all__ = [
     "AdditiveUniformSampler",
     "parametrised_correlation_matrix",
     "BivariateNormalSampler",
+    "BivariateUniformMarginsSampler",
+    "SwissRollSampler",
     "SplitMultinormal",
     "SplitStudentT",
     "TransformedSampler",
