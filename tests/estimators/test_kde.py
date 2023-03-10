4704be2b16168a40c7ee4d85737446bd41d61385
Paweł Czyż <pczyz@protonmail.com>
2023-03-10 14:21:34 +0100

    Add estimating differential entropies.

--- tests/estimators/test_kde.py
+++ tests/estimators/test_kde.py
@@ -9,7 +9,10 @@ def test_kde_estimator(n_points: int = 20_000, corr: float = 0.8) -> None:
     sampler = BivariateUniformMarginsSampler(gaussian_correlation=corr)
     x, y = sampler.sample(n_points=n_points, rng=0)
 
-    estimator = KDEMutualInformationEstimator()
-    estimate = estimator.estimate(x, y)
+    estimator = KDEMutualInformationEstimator(standardize=False)
+    result = estimator.estimate_entropies(x, y)
 
-    assert estimate == pytest.approx(sampler.mutual_information())
+    assert result.entropy_x == pytest.approx(0)
+    assert result.entropy_y == pytest.approx(0)
+
+    assert result.mutual_information == pytest.approx(sampler.mutual_information())
