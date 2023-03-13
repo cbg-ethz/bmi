b21b86d1407149150a450df82ae964c9d63773de
Paweł Czyż <pczyz@protonmail.com>
2023-03-13 10:04:46 +0100

    Change hyperparameters.

--- tests/estimators/test_kde.py
+++ tests/estimators/test_kde.py
@@ -1,21 +1,31 @@
 """Tests of the kernel density estimator."""
+import numpy as np
 import pytest
 
 from bmi.estimators.api import KDEMutualInformationEstimator
-from bmi.samplers.api import BivariateUniformMarginsSampler
+from bmi.samplers.api import BivariateNormalSampler
 
 
-def test_kde_estimator(n_points: int = 10_000, corr: float = 0.9) -> None:
-    sampler = BivariateUniformMarginsSampler(gaussian_correlation=corr)
+def test_kde_estimator(n_points: int = 10_000, corr: float = 0.8) -> None:
+    sampler = BivariateNormalSampler(correlation=corr)
     x, y = sampler.sample(n_points=n_points, rng=0)
 
     # standardize=False, so that we have differential entropies of uniform(0, 1)=0
     estimator = KDEMutualInformationEstimator(
-        standardize=False, bandwidth_xy=0.1, kernel_xy="cosine"
+        standardize=False, bandwidth_xy="scott", kernel_xy="gaussian"
     )
     result = estimator.estimate_entropies(x, y)
 
-    assert result.entropy_x == pytest.approx(0)
-    assert result.entropy_y == pytest.approx(0)
+    # Differential entropy of a Gaussian N(0, 1)
+    constant = 2 * np.pi * np.e
+    entropy_normal = 0.5 * np.log(constant)
+    # Differential entropy of the bivariate Gaussian
+    entropy_joint = 0.5 * np.log(constant**2 * (1 - corr**2))
+
+    assert result.entropy_x == pytest.approx(entropy_normal, rel=0.05)
+    assert result.entropy_y == pytest.approx(entropy_normal, rel=0.05)
+    assert result.entropy_xy == pytest.approx(entropy_joint, rel=0.05)
 
     assert result.mutual_information == pytest.approx(sampler.mutual_information())
+
+    assert result.mutual_information == pytest.approx(estimator.estimate(x, y))
