511c0dd0f301e371a139a959c041b371236ce38f
Paweł Czyż <pczyz@protonmail.com>
2023-04-25 12:55:01 +0200

    Sketch the API.

--- src/bmi/samplers/_dispersion.py
+++ src/bmi/samplers/_dispersion.py
@@ -1,4 +1,7 @@
 """Utilities for creating dispersion matrices."""
+import dataclasses
+from typing import Optional
+
 import numpy as np
 
 
@@ -67,3 +70,132 @@ def parametrised_correlation_matrix(
             corr_matrix[j, i] = correlation_y
 
     return corr_matrix
+
+
+@dataclasses.dataclass
+class GaussianLVMParametrization:
+    """Parameters of a Gaussian latent variable models.
+
+    Attrs:
+        dim_x: the dimensionality of the X variable
+        dim_y: the dimensionality of the Y variable
+        n_interacting: number of strongly interacting components of X vs Y
+          (strength controlled by parameter `lambd`)
+        alpha: dense interactions (between every pair of variables)
+        beta_x: dense interactions between X components
+        beta_y: dense interactions between Y components
+        lambd: strength of strongly interacting components of X vs Y
+        epsilon_x: individual noise, increasing variances of X components
+        epsilon_y: individual noise, increasing variances of Y components
+        eta_x: increases variance of X components which are not strongly interacting
+          via `n_interacting` and `lambd`
+        eta_y: increases variance of Y components which are not strongly interacting
+          via `n_interacting` and `lambd`
+    """
+
+    dim_x: int
+    dim_y: int
+    n_interacting: int = 0
+    alpha: float = 0.0
+    beta_x: float = 0.0
+    beta_y: float = 0.0
+    lambd: float = 0.0
+    epsilon_x: float = 1.0
+    epsilon_y: Optional[float] = None
+    eta_x: float = 0.0
+    eta_y: Optional[float] = None
+
+    def mixing(self) -> np.ndarray:
+        """"""
+        pass
+
+    def covariance(self) -> np.ndarray:
+        pass
+
+    def correlation(self) -> np.ndarray:
+        pass
+
+    def latent_variable_labels(self) -> list[str]:
+        pass
+
+    def xy_labels(self) -> list[str]:
+        pass
+
+
+class DenseLVMParametrization(GaussianLVMParametrization):
+    """Dense interaction scheme: between every two (distinct) variables
+    the covariance is the same.
+    """
+
+    def __init__(self, dim_x: int, dim_y: int, alpha: float, epsilon: float) -> None:
+        super().__init__(
+            dim_x=dim_x,
+            dim_y=dim_y,
+            n_interacting=0,
+            alpha=alpha,
+            beta_x=0.0,
+            beta_y=0.0,
+            lambd=0.0,
+            epsilon_x=epsilon,
+            epsilon_y=epsilon,
+            eta_x=0.0,
+            eta_y=0.0,
+        )
+
+    def correlation_strength(self) -> float:
+        """Apart from correlations Cor(X_i, X_i)=Cor(Y_i, Y_i)=1
+        all the correlations are the same and given by this value.
+        """
+        return self.alpha**2 / (self.alpha**2 + self.epsilon_x**2)
+
+
+class SparseLVMParametrization(GaussianLVMParametrization):
+    def __init__(
+        self,
+        dim_x: int,
+        dim_y: int,
+        n_interacting: int,
+        beta: float = 0.0,
+        lambd: float = 1.0,
+        epsilon: float = 1.0,
+        eta: Optional[float] = None,
+    ) -> None:
+        """
+
+        Args:
+            eta: by default `lambd` will be used, so that
+              all variables have the same variance
+        """
+        eta = lambd if eta is None else eta
+
+        super().__init__(
+            dim_x=dim_x,
+            dim_y=dim_y,
+            n_interacting=n_interacting,
+            alpha=0.0,
+            beta_x=beta,
+            beta_y=beta,
+            lambd=lambd,
+            epsilon_x=epsilon,
+            epsilon_y=epsilon,
+            eta_x=eta,
+            eta_y=eta,
+        )
+
+    def xx_correlation(self) -> float:
+        """Correlation between X_i and X_j for i *different from* j
+        (for i=j the correlation is obviously 1).
+        """
+        pass
+
+    def yy_correlation(self) -> float:
+        """Correlation between X_i and X_j for i *different from* j
+        (for i=j the correlation is obviously 1).
+        """
+        pass
+
+    def xy_interacting_correlation(self) -> float:
+        """Correlation between X_i and Y_i for i < `n_interacting`.
+        (All the other correlations X_i and Y_j are 0).
+        """
+        pass
