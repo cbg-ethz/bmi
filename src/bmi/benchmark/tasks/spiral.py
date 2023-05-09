6b9827c25822deaa6993bf5cc029036f4d398e44
Frederic Grabowski <grabowski.frederic@gmail.com>
2023-05-09 13:41:01 +0200

    normalize spiral speed

--- src/bmi/benchmark/tasks/spiral.py
+++ src/bmi/benchmark/tasks/spiral.py
@@ -7,8 +7,9 @@ from bmi.benchmark.task import Task
 
 def transform_spiral_task(
     base_task: Task,
-    speed: float = 1 / 3,
+    speed: float = 1.0,
     task_name: Optional[str] = None,
+    normalize_speed: bool = True,
 ) -> Task:
     assert base_task.dim_x > 1, "dim_x has to be at least 2 for rotation to exist"
     assert base_task.dim_y > 2, "dim_y has to be at least 3 for rotation to exist"
@@ -18,8 +19,15 @@ def transform_spiral_task(
     generator_x = rt.so_generator(base_task.dim_x, 0, 1)
     generator_y = rt.so_generator(base_task.dim_y, 1, 2)
 
-    transform_x = rt.Spiral(generator=generator_x, speed=speed)
-    transform_y = rt.Spiral(generator=generator_y, speed=speed)
+    if normalize_speed:
+        speed_x = speed / base_task.dim_x
+        speed_y = speed / base_task.dim_y
+    else:
+        speed_x = speed
+        speed_y = speed
+
+    transform_x = rt.Spiral(generator=generator_x, speed=speed_x)
+    transform_y = rt.Spiral(generator=generator_y, speed=speed_y)
 
     spiral_sampler = samplers.TransformedSampler(
         base_sampler=base_sampler,
@@ -31,5 +39,5 @@ def transform_spiral_task(
         sampler=spiral_sampler,
         task_id=f"spiral-{base_task.id}",
         task_name=task_name or f"Spiral @ {base_task.name}",
-        task_params=base_task.params | {"speed": speed},
+        task_params=base_task.params | {"speed": speed, "normalize_speed": normalize_speed},
     )
