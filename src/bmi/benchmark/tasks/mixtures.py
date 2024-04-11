3d6e5f1afb875cf123f2c8884b61ed425e0d69eb
Frederic Grabowski <grabowski.frederic@gmail.com>
2024-04-11 16:12:46 +0200

    add concentric multinormal and inlier tasks

--- src/bmi/benchmark/tasks/mixtures.py
+++ src/bmi/benchmark/tasks/mixtures.py
@@ -182,3 +182,88 @@ def task_waves(
             "wave_frequency": wave_frequency,
         },
     )
+
+
+def task_concentric_multinormal(
+    dim_x,
+    n_components=3,
+    mi_estimate_sample=_MC_MI_ESTIMATE_SAMPLE,
+) -> Task:
+    """Isotropic Gaussians with varying standard deviation."""
+
+    assert n_components > 0
+
+    dist = fine.mixture(
+        proportions=jnp.ones(n_components) / n_components,
+        components=[
+            fine.MultivariateNormalDistribution(
+                covariance=jnp.diag(jnp.array(dim_x * [i**2] + [0.0001])),
+                mean=jnp.array(dim_x * [0.0] + [1.0 * i]),
+                dim_x=dim_x,
+                dim_y=1,
+            )
+            for i in range(1, 1 + n_components)
+        ],
+    )
+    sampler = fine.FineSampler(dist, mi_estimate_sample=mi_estimate_sample)
+
+    return Task(
+        sampler=sampler,
+        task_id=f"{dim_x}v1-concentric_gaussians-{n_components}",
+        task_name=f"Concentric {dim_x} × 1",
+        task_params={
+            "n_components": n_components,
+        },
+    )
+
+
+def task_multinormal_sparse_w_inliers(
+    dim_x,
+    dim_y,
+    n_interacting: int = 2,
+    strength: float = 2.0,
+    inlier_fraction: float = 0.2,
+    mi_estimate_sample=_MC_MI_ESTIMATE_SAMPLE,
+) -> Task:
+
+    assert 0.0 <= inlier_fraction <= 1.0
+
+    params = samplers.GaussianLVMParametrization(
+        dim_x=dim_x,
+        dim_y=dim_y,
+        n_interacting=n_interacting,
+        alpha=0.0,
+        lambd=strength,
+        beta_x=0.0,
+        eta_x=strength,
+    )
+
+    signal_dist = fine.MultivariateNormalDistribution(
+        dim_x=dim_x,
+        dim_y=dim_y,
+        covariance=params.correlation,
+    )
+
+    noise_dist = fine.ProductDistribution(
+        dist_x=signal_dist.dist_x,
+        dist_y=signal_dist.dist_y,
+    )
+
+    dist = fine.mixture(
+        proportions=jnp.array([1 - inlier_fraction, inlier_fraction]),
+        components=[signal_dist, noise_dist],
+    )
+
+    sampler = fine.FineSampler(dist, mi_estimate_sample=mi_estimate_sample)
+
+    task_id = f"mult-sparse-w-inliers-{dim_x}-{dim_y}-{n_interacting}-{strength}-{inlier_fraction}"
+    return Task(
+        sampler=sampler,
+        task_id=task_id,
+        task_name=f"Multinormal {dim_x} × {dim_y} (sparse, {inlier_fraction:.0%} inliers)",
+        task_params={
+            "n_interacting": n_interacting,
+            "strength": strength,
+            "inlier_fraction": inlier_fraction,
+        },
+    )
